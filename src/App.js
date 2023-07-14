6a0c445c85032cc78c81941c960c6310ea3a7433
Shreyas <shreyaskartikey25@gmail.com>
2023-07-14 22:43:00 +0530

    My project pathFinder

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import PathfindingVisualiser from './PathfindingVisualiser/PathfindingVisualiser';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Path Finding Visualiser</h1>
+      <PathfindingVisualiser></PathfindingVisualiser>
+      <h3>-- Developed by Shreyas Kartikey --</h3>
+    </div>
+  );
+}
+
+export default App;
+
